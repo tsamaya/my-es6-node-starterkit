b2db2db124bc447e3b53160d183a9a292201ab77
arnaud ferrand <arferrand@gmail.com>
2018-08-10 13:00:12 +0200

    add a node sample

---
+++ samples/node-sample.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+const mathLib = require('../dist/es6-node-starterkit');
+
+console.log('mathLib.sum(1, 2)) =', mathLib.sum(1, 2));
+console.log('mathLib.diff(3, 1)) =', mathLib.diff(3, 1));
